7f126c086cedb5456b0fb1bd3fa234e390b2eb1a
Mohamed Riahi <mohamed.riahi.sbc@gmail.com>
2026-07-10 18:17:21 +0300

    Fix imports in curriculum-data index

--- curriculum-data/elementary/index.ts
+++ curriculum-data/elementary/index.ts
@@ -1,7 +1,7 @@
-import { grade1 } from "./elementary/grade1";
-import { Grade } from "./types";
+// بدلاً من ./elementary/grade1 احذف كلمة elementary لأنك في نفس المجلد
+import { grade1 } from "./grade1"; 
+import { Grade } from "../types"; // بما أن types موجودة في المجلد الأب curriculum-data
 
 export const curriculumData: Record<string, Grade> = {
   "grade1": grade1,
-  // يمكنك إضافة grade2, grade3 هنا لاحقاً بنفس الطريقة
 };
